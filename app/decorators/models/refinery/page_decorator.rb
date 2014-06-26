fb32481a342c821f6fdeaf3e4a304ab050d9c5ee
Anita Graham <anita@joli.com.au>
2014-06-26 17:06:28 +0800

    add a before_save to modify testimonial page_part

--- app/decorators/models/refinery/page_decorator.rb
+++ app/decorators/models/refinery/page_decorator.rb
@@ -3,4 +3,11 @@ Refinery::Page.class_eval do
   # Whitelist the testimonials control fields
   attr_accessible :testimonials_show, :testimonials_count, :testimonials_select
   validates :testimonials_count, :numericality => { :only_integer => true, :greater_than_or_equal_to=>0 }
+  before_save testimonial_page_part
+
+  def testimonial_page_part
+    testimonial_part = @page.part_with_title('testimonials')
+    @page.parts << {title: 'testimonials', position: @page.parts.count} if @page.testimonials_show && !testimonial_part
+    @page.parts.delete(testimonial_part) if testimonial_part && !@page.testimonials_show
+  end
 end
