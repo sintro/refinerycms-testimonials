96aedb6e58d2d3510d391a0dd46cf1eda455c894
Anita Graham <anita@joli.com.au>
2015-02-18 20:47:29 +0800

    Remove obsolete(?) Refinery.i18=n_enabled

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
-(Refinery.i18n_enabled? ? Refinery::I18n.frontend_locales : [:en]).each do |lang|
-  I18n.locale = lang
-
   if defined?(Refinery::User)
     Refinery::User.all.each do |user|
       if user.plugins.where(:name => 'refinerycms-testimonials').blank?
@@ -9,4 +6,3 @@
       end
     end
   end
-end
