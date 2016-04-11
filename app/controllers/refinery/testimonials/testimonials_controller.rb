7a7e91e1e4cf7b95fc0fe1fe350eb4215e447d90
sintro <skipintro@mail.ru>
2016-04-11 14:21:35 +0300

    locale fix

--- app/controllers/refinery/testimonials/testimonials_controller.rb
+++ app/controllers/refinery/testimonials/testimonials_controller.rb
@@ -9,14 +9,14 @@ module Refinery
 
 
         def show
-          @testimonial = Refinery::Testimonials::Testimonial.with_translations(I18n.locale).friendly.find(params[:id])
+          @testimonial = Refinery::Testimonials::Testimonial.with_translations(::I18n.locale).friendly.find(params[:id])
 		  @testimonial_images = @testimonial.images.includes(:translations)
         end
 
         protected
 
 		def load_testimonials
-		  @testimonials = Refinery::Testimonials::Testimonial.with_translations(I18n.locale)
+		  @testimonials = Refinery::Testimonials::Testimonial.with_translations(::I18n.locale)
 		                    .reorder(received_date: :desc)
 		                    .references(:image_pages)
 		                    .includes(:images, images: [:translations])
